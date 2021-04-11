bd57b8241a6b4bf4d389e572322ec0afde50b193
abhishek.chaturvedi <abhishekchaturvedi789@gmail.ccom>
2021-04-12 00:26:45 +0530

    truthy and falsy values examples

--- functions.test.js
+++ functions.test.js
@@ -1,8 +1,35 @@
 const { expect } = require('@jest/globals')
 const functions = require('./functions')
 
+//toEqual
 test('should return addition of number', () => {
     expect(
         functions.add(1, 2)
     ).toEqual(3)
 })
+
+//toBeNull
+test('should return Null', () => {
+    expect(
+        functions.isNull()
+    ).toBeNull()
+})
+
+//toBeFalsy
+test('should be falsy', () => {
+    expect(
+        functions.checkValue(null)
+    ).toBeFalsy()
+})
+
+test('should be Abhishek Chaturvedi', () => {
+    expect(
+        functions.createUser()
+    ).toEqual({
+        firstName: 'Abhishek',
+        lastName: 'Chaturvedi'
+    })
+})
+
+
+
